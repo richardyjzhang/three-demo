e7d98065bf2ee5492c0fbc093f83e2200340d345
zhangyj <richard.yujia.zhang@gmail.com>
2022-08-02 13:43:45 +0800

    小球上下浮动

--- src/pages/floating-ball/index.tsx
+++ src/pages/floating-ball/index.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import TWEEN from "@tweenjs/tween.js";
+import React, { useEffect } from "react";
+import * as THREE from "three";
+import styles from "./index.css";
+import init from "./init";
+
+const { scene, camera, renderer, orbitControl } = init();
 
 const FloatingBallPage: React.FC = () => {
-  return <div>FLOATING BALL</div>;
+  // 自身div
+  const divRef = React.createRef<HTMLDivElement>();
+
+  // 动画ID
+  let idAnimateFrame = 0;
+
+  // 逐帧渲染
+  const animate = () => {
+    idAnimateFrame = requestAnimationFrame(animate);
+    orbitControl.update();
+    TWEEN.update();
+    renderer.render(scene, camera);
+  };
+
+  // 创造漂浮球
+  const createFloatingBall = () => {
+    const geo = new THREE.SphereGeometry(5, 64, 64);
+    const material = new THREE.MeshBasicMaterial({
+      color: "#ff0000",
+    });
+    const ball = new THREE.Mesh(geo, material);
+    scene.add(ball);
+
+    // 动画参数
+    const paras = {
+      y: 0,
+    };
+    const tween = new TWEEN.Tween(paras)
+      .to(
+        {
+          y: 15,
+        },
+        2000
+      )
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .onUpdate(() => {
+        ball.position.y = paras.y;
+      })
+      .yoyo(true)
+      .repeat(Infinity);
+    tween.start();
+  };
+
+  useEffect(() => {
+    if (!divRef.current) return;
+
+    renderer.setSize(divRef.current.clientWidth, divRef.current.clientHeight);
+    animate();
+    const canvas = divRef.current.appendChild(renderer.domElement);
+
+    createFloatingBall();
+
+    return () => {
+      cancelAnimationFrame(idAnimateFrame);
+      divRef.current?.removeChild(canvas);
+    };
+  }, [divRef]);
+
+  return <div className={styles.root} ref={divRef} />;
 };
 
 export default FloatingBallPage;
